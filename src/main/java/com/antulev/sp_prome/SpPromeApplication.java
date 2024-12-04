b9e7c13a8a3e42d8b18c1ed3658db8a123f94efb
Le Tu <levantu.13139@gmail.com>
2024-12-04 20:28:17 +0700

    Add files via upload

---
+++ src/main/java/com/antulev/sp_prome/SpPromeApplication.java
@@ -0,0 +1,13 @@
+package com.antulev.sp_prome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpPromeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpPromeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antulev/sp_prome/SpPromeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antulev.sp_prome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpPromeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
